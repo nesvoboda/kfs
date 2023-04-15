b5edefdcdaf97b3cdbc1e6dc0192017111d133aa
Arseniy Shishaev <ashishae@student.42.fr>
2023-04-15 20:07:39 +0200

    Fix

--- includes/text_handler.h
+++ includes/text_handler.h
@@ -1,12 +1,14 @@
 #ifndef TEXT_HANDLER_H
 # define TEXT_HANDLER_H
 
+#include "screen.h"
+
 typedef struct text_char {
     char c;
     uint8_t color;
 } text_char_t;
 
-void insert_character(text_char_t c, int index, int screen_no)
+void insert_character(text_char_t c, int index, int screen_no);
 void erase(int index, int screen_no);
 void remove_character(int index, int screen_no);
 text_char_t* get_text(screen_no); 
