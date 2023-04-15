039af63cca40f5b6e7044ae61be1775379bd8497
Artem BLANAR <ablanar@e1r2p15.clusters.42paris.fr>
2023-04-15 20:57:02 +0200

    keyboard_handler

--- idt/keyboard.c
+++ idt/keyboard.c
@@ -1,68 +1,69 @@
 #include "idt.h"
+#include "direction.h"
+#include "keyboard.h"
 
-const unsigned char kbdus[128] =
+
+void handle_scroll(enum direction dir)
 {
-    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
-  '9', '0', '-', '=', '\b',	/* Backspace */
-  '\t',			/* Tab */
-  'q', 'w', 'e', 'r',	/* 19 */
-  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
-    0,			/* 29   - Control */
-  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
- '\'', '`',   0,		/* Left shift */
- '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
-  'm', ',', '.', '/',   0,				/* Right shift */
-  '*',
-    0,	/* Alt */
-  ' ',	/* Space bar */
-    0,	/* Caps lock */
-    0,	/* 59 - F1 key ... > */
-    0,   0,   0,   0,   0,   0,   0,   0,
-    0,	/* < ... F10 */
-    0,	/* 69 - Num lock*/
-    0,	/* Scroll Lock */
-    0,	/* Home key */
-    0,	/* Up Arrow */
-    0,	/* Page Up */
-  '-',
-    0,	/* Left Arrow */
-    0,
-    0,	/* Right Arrow */
-  '+',
-    0,	/* 79 - End key*/
-    0,	/* Down Arrow */
-    0,	/* Page Down */
-    0,	/* Insert Key */
-    0,	/* Delete Key */
-    0,   0,   0,
-    0,	/* F11 Key */
-    0,	/* F12 Key */
-    0,	/* All other keys are undefined */
-};
+    screen_handle_scroll(dir, current_screen);
+}
 
-#define KBD_DATA_REG        0x60    /* I/O port for keyboard data */
-#define KBD_SCANCODE_MASK   0x7f
-#define KBD_STATUS_MASK     0x80
+void erase()
+{
+  screen_erase(current_screen);
+}
+
+void next_screen()
+{
+  if (++current_screen == SCREEN_MAX)
+    current_screen = 0;
+}
+
+void switch_color(int color)
+{
+  current_color = color;
+}
+
+void add_char(char c)
+{
+  screen_add_char(c, current_color, current_screen);
+}
+
 
 void keyboard_handler(registers_t regs)
 {
+  
 	char scancode;
 	scancode = inb(KBD_DATA_REG);
-	ft_putnbr(scancode);
+  if ((scancode & KBD_STATUS_MASK) == 0)
+  {
 
-   if ((scancode & KBD_STATUS_MASK) == 0) {
-      ft_putnbr(scancode);
-      if (scancode == 1)
-      {
-        terminal_writestring('switch to first');
-        switch_to_second_terminal_buffer();
-      }
-      if (scancode  == 15)
-      {
-        terminal_writestring('switch to second');
-
-        switch_to_first_terminal_buffer();
-      }
-      terminal_putchar(kbdus[scancode & KBD_SCANCODE_MASK]);
-   }
+    if (keyboard_flags.arrow_flag && scancode == UP_PRESSED)
+      handle_scroll(TOP);
+    else if (keyboard_flags.arrow_flag && scancode == BOTTOM_PRESSED)
+      handle_scroll(BOTTOM);
+    else if (keyboard_flags.arrow_flag && scancode ==  LEFT_PRESSED)
+      handle_scroll(LEFT);
+    else if (keyboard_flags.arrow_flag && scancode == RIGHT_PRESSED)
+      handle_scroll(RIGHT);
+    else if (keyboard_flags.arrow_flag && scancode == RIGHT_PRESSED)
+      handle_scroll(RIGHT);
+    else if (scancode == ALT)
+      keyboard_flags.alt_flag = 1;
+    else if (scancode == TAB && keyboard_flags.alt_flag)
+      switch_screen();
+    else if ((scancode >= 2 && scancode <= 9) && keyboard_flags.alt_flag)
+      switch_color(scancode - 1);
+    else if (scancode == BACKSPAPCE)
+      erase();
+    else
+      add_char(kbdus[scancode & KBD_SCANCODE_MASK]);
+  }
+  else   
+  {
+    if (scancode == ALT_REALEASE)
+      keyboard_flags.alt_flag = 0;
+    if (scancode == ARROW)
+      keyboard_flags.arrow_flag = !keyboard_flags.arrow_flag;
+  }
 }
