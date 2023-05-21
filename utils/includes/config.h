e29c4348a2b811d4515f24091ac91608d9ef3b33
Arseniy Shishaev <ashishae@student.42.fr>
2023-05-21 15:32:29 +0200

    Fix incorrect copy

--- utils/includes/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef CONFIG_H
-# define CONFIG_H
-
-#define SCREEN_MAX 5
-
-#endif
--- utils/includes/timer.h
+++
@@ -1,9 +0,0 @@
-#ifndef TIMER_H
-#define TIMER_H
-#include "idt.h"
-#include "types.h"
-
-void init_timer(u32int frequency);
-void sleep(u32int time);
-u32int gettick();
-#endif
