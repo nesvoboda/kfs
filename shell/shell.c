8133f1d9c0474736f0dad8dc5105fedcd78e25b0
Arseniy Shishaev <ashishae@student.42.fr>
2023-06-28 21:42:12 +0200

    Add shell help text

--- shell/shell.c
+++ shell/shell.c
@@ -14,23 +14,33 @@ void halt()
 	outw(0x604, 0x2000);
 }
 
+char* help_message = "Welcome to blashOS!\n\
+Print \"help\" to see this message.\n\
+Available commands:\n\
+\n\
+halt - shutdown the system\n\
+reboot - reboot the system\n\
+backtrace - show a stack trace with symbols\n\
+help - show this message\n";
+
 void shell()
 {
 	// char *line;
 	char buf;
+	printf("%s\n", help_message);
 	while (1) {
 		write(1, "> ", 2);
 		flush_line();
-		int ret = read(0, line, 99);
+		int ret = read(0, line, 3000);
 
-		if (!ft_strncmp(line, "kek", 3))
-			write(1, "kek\n", 4);
-		else if (!ft_strncmp(line, "halt", 4))
+		if (!ft_strncmp(line, "halt", 4))
 			halt();
 		else if (!ft_strncmp(line, "reboot", 6))
 			reboot();
 		else if (!ft_strncmp(line, "backtrace", 9))
 			backtrace(15);
+		else if (!ft_strncmp(line, "help", 4))
+			printf("%s\n", help_message);
 		else
 			write(1, "Unknown command\n", 16);
 	}
