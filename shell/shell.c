961e030b0a1508d2e011c5292f043de58ea6f7d7
Artem BLANAR <ablanar@paul-f4Br4s3.clusters.42paris.fr>
2023-06-28 22:17:19 +0200

    new verify command

--- shell/shell.c
+++ shell/shell.c
@@ -4,7 +4,7 @@ char line[100];
 
 void flush_line()
 {
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 3000; i++) {
 		line[i] = '\0';
 	}
 }
@@ -23,6 +23,11 @@ reboot - reboot the system\n\
 backtrace - show a stack trace with symbols\n\
 help - show this message\n";
 
+int verify_command(size_t len, char *command, char *input)
+{
+	return (!ft_strncmp(command, input, len + 1));
+}
+
 void shell()
 {
 	// char *line;
@@ -33,15 +38,15 @@ void shell()
 		flush_line();
 		int ret = read(0, line, 3000);
 
-		if (!ft_strncmp(line, "halt", 4))
+		if (verify_command(4, "halt", line))
 			halt();
-		else if (!ft_strncmp(line, "reboot", 6))
+		else if (verify_command(6, "reboot", line))
 			reboot();
-		else if (!ft_strncmp(line, "backtrace", 9))
+		else if (verify_command(9, "backtrace", line))
 			backtrace(15);
-		else if (!ft_strncmp(line, "help", 4))
+		else if (verify_command(4, "help", line))
 			printf("%s\n", help_message);
 		else
-			write(1, "Unknown command\n", 16);
+			printf("command not found: %s\n", line);
 	}
 }
