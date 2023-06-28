177fb9708277fbdddba51b589a525db13819c9f2
Arseniy Shishaev <ashishae@student.42.fr>
2023-06-28 21:41:57 +0200

    Improve backtrace

--- backtrace/backtrace.c
+++ backtrace/backtrace.c
@@ -11,9 +11,13 @@ void backtrace(unsigned int maxFrames)
 
 	for (unsigned int frame = 0; stk && frame < maxFrames; ++frame) {
 		u32int addr = *((uint32_t*)(stk->eip - 0x4)) + stk->eip;
-		name = addr2name(addr);
-		write(1, name, ft_strlen(name));
-		write(1, "\n", 1);
+		name = addr2name((void * )addr);
+		if (!ft_strncmp(name, "kernel_main", 11)) {
+			printf("In 0x%x: %s\n", addr, name);	
+		} else {
+			printf("In 0x%x: %s + 0x%x\n", addr, name, (u32int) addr - stk->eip);
+		}
+		
 		stk = stk->ebp;
 	}
 }
