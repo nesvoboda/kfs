0f457a220a3f738799bdcdd2421b0f2284c95cd3
Artem Blanar <blanar.a.v@gmail.com>
2023-09-10 18:07:44 +0200

    brk

--- paging/brk.c
+++ paging/brk.c
@@ -15,12 +15,7 @@ int brk(void *end_address)
 
     for (uintptr_t i = (uintptr_t)new_hole_address; i < (uintptr_t)end_address; i += 0x1000)
         _alloc_frame(&current_manager, i, 0, 1);
-
-    new_hole_address->size =  usable_size(end_address - (kheap.data + kheap.size));
-    footer_t *new_footer = (void *)new_hole_address + new_hole_address->size + sizeof(header_t);
-    new_footer->to_header = new_hole_address;
-    kheap.size =  end_address - kheap.data;
-    deallocate(&kheap, (void *)new_hole_address + sizeof(header_t));
+    extend_heap(&kheap, end_address);
     return 0;
 }
 
